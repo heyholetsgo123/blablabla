4d1e2de204d24a18f54270b63fb4b48a684b8399
blabla <heyholetsgo4@protonmail.com>
2023-01-18 08:41:49 +0200

    'a'

--- src/utils.py
+++ src/utils.py
@@ -13,13 +13,14 @@ import zipfile
 
 import shutil
 import tempfile
-from random_user_agent.user_agent import UserAgent
-from random_user_agent.params import SoftwareName, OperatingSystem
 
-software_names = [SoftwareName.CHROME.value]
-operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]   
+# from random_user_agent.user_agent import UserAgent
+# from random_user_agent.params import SoftwareName, OperatingSystem
 
-user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+# software_names = [SoftwareName.CHROME.value]
+# operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]   
+
+# user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
 
 class ProxyExtension:
     manifest_json = """
