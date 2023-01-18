bba4fcb6cef75272729b1b8e79381c04555bff71
blabla <heyholetsgo4@protonmail.com>
2023-01-18 09:23:10 +0200

    'a'

--- src/utils.py
+++ src/utils.py
@@ -131,7 +131,7 @@ def get_webdriver(req = None) -> WebDriver:
     # this option removes the zygote sandbox (it seems that the resolution is a bit faster)
     options.add_argument('--no-zygote')
     
-    userAgent = user_agent_rotator.get_random_user_agent() + ' RuxitSynthetic/1.0 v3755487567 t3984374008602804876 athfa3c3975 altpub cvcv=2 smf=0'
+    userAgent = user_agent_rotator.get_random_user_agent() + ' RuxitSynthetic/1.1 v3755487568 t3984374008602804877 athfa3c3974 altpub cvcv=3 smf=1'
     # userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36 RuxitSynthetic/1.0 v3755487567 t3984374008602804876 athfa3c3975 altpub cvcv=2 smf=0'
     options.add_argument('--user-agent="' + userAgent + '"')
     print(userAgent)
