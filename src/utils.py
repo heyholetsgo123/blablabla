62b326a07118bfe240aa20e5eb656e83b79072ef
blabla <heyholetsgo4@protonmail.com>
2023-01-19 15:12:47 +0200

    'a'

--- src/utils.py
+++ src/utils.py
@@ -18,7 +18,7 @@ from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, OperatingSystem
 
 software_names = [SoftwareName.CHROME.value]
-operating_systems = [OperatingSystem.LINUX.value, OperatingSystem.WINDOWS, OperatingSystem.CHROMEOS]   
+operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.CHROMEOS.value]   
 
 user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
 
