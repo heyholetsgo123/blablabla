eb77db09b00447383847daf0514f0e7e940e6817
blabla <heyholetsgo4@protonmail.com>
2023-01-22 09:44:28 +0200

    'a'

--- src/utils.py
+++ src/utils.py
@@ -138,7 +138,7 @@ def get_webdriver(req = None) -> WebDriver:
     #working - userAgent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36 RuxitSynthetic/1.0 v4383360167508159838 t8052286838287810618'
     # userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0'
     
-    options.add_argument('--user-agent=' + userAgent + '')
+    #options.add_argument('--user-agent=' + userAgent + '')
     print(userAgent)
 
     if req is not None and req.proxy is not None:
