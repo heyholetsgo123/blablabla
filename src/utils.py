04f9116f3f66481f684d7d24a7d59153b72dc991
blabla <heyholetsgo4@protonmail.com>
2023-01-22 09:27:21 +0200

    'a'

--- src/utils.py
+++ src/utils.py
@@ -124,11 +124,11 @@ def get_webdriver(req = None) -> WebDriver:
 
     # undetected_chromedriver
     options = uc.ChromeOptions()
-    # options.add_argument('--no-sandbox')
+    options.add_argument('--no-sandbox')
     options.add_argument('--window-size=1920,1080')
     # todo: this param shows a warning in chrome head-full
-    # options.add_argument('--disable-setuid-sandbox')
-    # options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-setuid-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     # this option removes the zygote sandbox (it seems that the resolution is a bit faster)
     # options.add_argument('--no-zygote')
     
