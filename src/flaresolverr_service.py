a36114715a1c6c671a17d8baf953815dc04aa20c
blabla <heyholetsgo4@protonmail.com>
2023-01-23 19:59:08 +0200

    'a'

--- src/flaresolverr_service.py
+++ src/flaresolverr_service.py
@@ -231,7 +231,7 @@ def _evil_logic(req: V1RequestBase, driver: WebDriver, method: str) -> Challenge
         while True:
             try:
                 driver.switch_to.frame(driver.find_element(By.XPATH, '//*[contains(@title, "Widget containing")]'))
-                driver.find_element(By.XPATH, '//*[@class="mark"]').click()
+                driver.find_element(By.XPATH, '//*[@id="checkbox"]').click()
                 driver.switch_to.default_content()
             except Exception as e: 
                 print(e)
