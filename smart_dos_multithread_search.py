08136d496c7618b56b3fac9036a6d59e34aae202
blabla <heyholetsgo4@protonmail.com>
2023-01-24 22:27:59 +0200

    'a'

--- smart_dos_multithread_search.py
+++ smart_dos_multithread_search.py
@@ -111,7 +111,7 @@ def attackUrlCF(baseUrl):
 			'Upgrade-Insecure-Requests': '1',
 			}
 
-			print(proxies)
+			print(proxies, cookiesStr)
 			print(requests.get('https://api.ipify.org', proxies=proxies).content)
 
 			requestsArr = []
