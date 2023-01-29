c6fc38dc98d6cc0df43a876e9f14ca971f98984d
blabla <heyholetsgo4@protonmail.com>
2023-01-29 22:23:07 +0200

    'a'

--- src/flaresolverr_service.py
+++ src/flaresolverr_service.py
@@ -285,8 +285,11 @@ def _evil_logic(req: V1RequestBase, driver: WebDriver, method: str) -> Challenge
     challenge_res.status = 200  # todo: fix, selenium not provides this info
     challenge_res.cookies = driver.get_cookies()
     challenge_res.userAgent = utils.get_user_agent(driver)
-    challenge_res.headers = driver.requests
-
+    # challenge_res.headers = driver.requests
+    for request in driver.requests:
+        print(request.url) # <--------------- Request url
+        print(request.headers) # <----------- Request headers
+        
     if not req.returnOnlyCookies:
           # todo: fix, selenium not provides this info
         challenge_res.response = driver.page_source
