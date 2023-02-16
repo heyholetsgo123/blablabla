1a461e4615e3a988449f21ec64e7576d45393a13
blabla <heyholetsgo4@protonmail.com>
2023-02-16 16:11:07 +0200

    'a'

--- smart_dos_funnel.py
+++ smart_dos_funnel.py
@@ -436,7 +436,7 @@ def attackSelenium(baseUrl):
 				while True:
 					try:
 						res = sendRequest(driver.current_url, proxies, userAgent, driver.get_cookies())
-						time.sleep(1)
+						time.sleep(10)
 						print(res)
 						if res == 500 and random.randint(1,3) == 1:
 							break
